ffd22a937e9fa17abf498200079c0d6f23eae8b8
ipxez <sleequil@gmail.com>
2020-11-19 03:19:51 +0000

    Created product objects

---
+++ app/js/lessons.js
@@ -0,0 +1,82 @@
+let lesson =    [
+    {
+        "id": 0001,
+        "name": "Networking",
+        "location": "London",
+        "price": 200,
+        "spaces": 5,
+        "image": "images/networking.jpg"
+    },
+    {
+        "id": 0002,
+        "name": "Cryptography Analysis",
+        "location": "Essex",
+        "price": 150,
+        "spaces": 5,
+        "image": "images/cryptography.jpeg"
+    },
+    {
+        "id": 0003,
+        "name": "Java",
+        "location": "London",
+        "price": 350,
+        "spaces": 5,
+        "image": "images/java.jpg"
+    },
+    {
+        "id": 0004,
+        "name": "Hacking",
+        "location": "Manchester",
+        "price": 700,
+        "spaces": 5,
+        "image": "images/hacking.jpg"
+    },
+    {
+        "id": 0005,
+        "name": "Data Science",
+        "location": "Essex",
+        "price": 550,
+        "spaces": 5,
+        "image": "images/data-science.png"
+    },
+    {
+        "id": 0006,
+        "name": "Web Development",
+        "location": "Essex",
+        "price": 475,
+        "spaces": 5,
+        "image": "images/webdev.jpg"
+    },
+    {
+        "id": 0007,
+        "name": "UX Design",
+        "location": "Essex",
+        "price": 300,
+        "spaces": 5,
+        "image": "images/ux-design.jpg"
+    },
+    {
+        "id": 0008,
+        "name": "Software Engineering",
+        "location": "Essex",
+        "price": 599,
+        "spaces": 5,
+        "image": "images/swe.jpg"
+    },
+    {
+        "id": 0009,
+        "name": "Robotics",
+        "location": "Essex",
+        "price": 950,
+        "spaces": 5,
+        "image": "images/robotics.jpg"
+    },
+    {
+        "id": 0010,
+        "name": "Game Development",
+        "location": "Birmingham",
+        "price": 999,
+        "spaces": 5,
+        "image": "images/game-dev.png"
+    }
+]
